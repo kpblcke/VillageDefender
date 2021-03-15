f728716af64c47a38eb9ed025c0bca4fd11406a3
kpblcke <kpblcke@gmail.com>
2021-03-15 14:26:54 +0400

    Initial commit

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private float minSpawnDelay = 1f;
+    [SerializeField]
+    private float maxSpawnDelay = 5f;
+
+    [SerializeField]
+    private Attacker attackerPrefab;
+    
+    private bool spawn = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartSpawning());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator StartSpawning()
+    {
+        yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+        while (spawn)
+        {
+            SpawnAttacker();
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+        }
+    }
+
+    void SpawnAttacker()
+    {
+        Instantiate(attackerPrefab, transform.position, transform.rotation);
+    }
+}
